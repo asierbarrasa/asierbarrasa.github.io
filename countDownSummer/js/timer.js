47b3d17ad0c9a7d29451462bc82152a1d67418b8
Barrasa Almeida <a.barrasa.almeida@accenture.com>
2021-04-21 14:28:28 +0200

    summer timer added

---
+++ countDownSummer/js/timer.js
@@ -0,0 +1,44 @@
+var countDownDate = new Date(2021,05,21)
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("displayTime").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if(days <30 && days >=0){
+    document.getElementById("displayFriday").innerHTML = "Ya casi es Navidad!";
+  }
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("displayTime").innerHTML = "YA ES NAVIDAD!";
+  }
+}, 1000);
+
+
+
+function getNextDayOfWeek(date, dayOfWeek) {
+    // Code to check that date and dayOfWeek are valid left as an exercise ;)
+
+    var resultDate = new Date(date.getTime());
+
+    resultDate.setDate(date.getDate() + (7 + dayOfWeek - date.getDay()) % 7);
+    resultDate.setHours(14);
+    resultDate.setMinutes(00);
+    resultDate.setSeconds(01);
+    return resultDate.getTime();
+}
