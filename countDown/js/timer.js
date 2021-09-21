90f309e64eba8238e2e21397cb2b9adc7a737245
Asier Barrasa Almeida <abarrasa@reappciona.net>
2021-09-21 15:47:48 +0200

    homer

--- countDown/js/timer.js
+++ countDown/js/timer.js
@@ -19,17 +19,38 @@ var x = setInterval(function() {
   document.getElementById("displayTime").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
+
+  document.getElementById("displayTime2").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  var htmlText = ''
   // If the count down is finished, write some text
-  if(days <3 && days >=0){
+  if(days < 3 && days >=0){
     document.getElementById("displayFriday").innerHTML = "Ya casi es viernes!";
+    if(document.getElementById("homer").classList.contains('hidden')){
+      document.getElementById("homer").classList.remove('hidden')
+      document.getElementById("timer").classList.add('hidden')
+
+    }
+
+
+  }else{
+    if(document.getElementById("timer").classList.contains('hidden')){
+      document.getElementById("timer").classList.remove('hidden')
+      document.getElementById("homer").classList.add('hidden')
+    }
   }
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("displayTime").innerHTML = "YA ES VIERNES!";
+    document.getElementById("displayFriday2").innerHTML = "Ya casi es viernes!";
+
   }
 }, 1000);
 
+function checkVisibility(toBeVisible){
 
+}
 
 function getNextDayOfWeek(date, dayOfWeek) {
     // Code to check that date and dayOfWeek are valid left as an exercise ;)
