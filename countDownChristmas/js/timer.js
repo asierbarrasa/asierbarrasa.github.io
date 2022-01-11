f601b0a6fbc1b2456be128dbb3d3471afdcb157a
Asier Barrasa <abarrasa@reappciona.net>
2022-01-11 11:21:10 +0100

    Actualizadas fechas

--- countDownChristmas/js/timer.js
+++ countDownChristmas/js/timer.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date(2021,11,25)
+var countDownDate = new Date(2022,11,25)
 
 // Update the count down every 1 second
 var x = setInterval(function() {
--- countDownTravel/js/timer.js
+++ countDownTravel/js/timer.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date(2021,11,03)
+var countDownDate = new Date(2022,0,28)
 
 // Update the count down every 1 second
 var x = setInterval(function() {
