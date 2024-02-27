109b6336bc8a4021766494cdf310ef0683f06f53
Asier Barrasa <abarrasa@reappciona.net>
2024-02-27 08:51:23 +0100

    Actualizado

--- countDownChristmas/js/timer.js
+++ countDownChristmas/js/timer.js
@@ -1,32 +1,32 @@
-var countDownDate = new Date(2022,11,25)
+var countDownDate = new Date(2024, 11, 25)
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+    // Get today's date and time
+    var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("displayTime").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+    // Display the result in the element with id="demo"
+    document.getElementById("displayTime").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text
-  if(days <30 && days >=0){
-    document.getElementById("displayFriday").innerHTML = "Ya casi es Navidad!";
-  }
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("displayTime").innerHTML = "YA ES NAVIDAD!";
-  }
+    // If the count down is finished, write some text
+    if (days < 30 && days >= 0) {
+        document.getElementById("displayFriday").innerHTML = "Ya casi es Navidad!";
+    }
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("displayTime").innerHTML = "YA ES NAVIDAD!";
+    }
 }, 1000);
 
 
